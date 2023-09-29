8fd71d0ca4ad93ee46fd1671036d8c427f43ddca
Harika-B8 <120396657+Harika-B8@users.noreply.github.com>
2023-09-29 14:57:28 +0530

    Add files via upload

---
+++ ds.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int a[5];
+printf("enter the elements: ");
+for(int i=0;i<=4;i++)
+{
+scanf("%d",&a[i]);
+for(int i=0;i<=4;i++)
+printf("%d",a[i]);
+}
+return 0;
+}
+
+
+
+
